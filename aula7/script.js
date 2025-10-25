316b5ab42368010490928ba706828d1ca1fd2137
C4ioDev <machado.caio.gomes@gmail.com>
2025-10-25 15:32:26 -0300

    mudança de formas

--- aula7/script.js
+++ aula7/script.js
@@ -1,36 +1,49 @@
-let cores = [
-    "blue",
-    "red",
-    "orange",
-    "green",
-    "yellow",
-    "grey",
-    "brown",
-    "purple",
-    "black",
-    "pink",
+// // let cores = [
+// //     "blue",
+// //     "red",
+// //     "orange",
+// //     "green",
+// //     "yellow",
+// //     "grey",
+// //     "brown",
+// //     "purple",
+// //     "black",
+// //     "pink",
+// ]
+
+let formas = [
+    "retangulo",
+    "circulo",
+    "quadrado",
+    "losango"
 ]
 
+
 let divPrincipal = document.querySelector("#principal")
-let pNome_Da_Cor = document.querySelector("#nome_da_cor")
+let pNome_Da_Forma = document.querySelector("#nome_da_cor")
 let index = 0
 
-pNome_Da_Cor.innerText = cores[index]
+pNome_Da_Forma.innerText = cores[index]
 
 function proximo(){
     index += 1
-    if (index > cores.length - 1){
+    if (index > formas.length - 1){
         index = 0
     }
-    pNome_Da_Cor.innerText = cores[index]
-    divPrincipal.style.backgroundColor = cores[index]
-
+    // pNome_Da_Formas.innerText = formas[index]
+    // divPrincipal.style.backgroundColor = formas[index]
+    if (formas[index] == "retangulo") {
+        divPrincipal.style.width = 400px
+         divPrincipal.style.height = 200px
+    } else if (formas[index] == "circulo") {
+        
+    }
 }
 function anterior(){
     index -= 1
     if (index < 0){
-        index = cores.length - 1
+        index = formas.length - 1
     }
-    pNome_Da_Cor.innerText = cores[index]
-    divPrincipal.style.backgroundColor = cores[index]
+    pNome_Da_Forma.innerText = formas[index]
+    divPrincipal.style.backgroundColor = formas[index]
 }
