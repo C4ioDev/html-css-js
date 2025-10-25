c91456a100694b1bbe626adc417b0866fe4b758d
C4ioDev <machado.caio.gomes@gmail.com>
2025-10-25 15:15:08 -0300

    caixa de cores

--- aula7/script.js
+++ aula7/script.js
@@ -4,5 +4,33 @@ let cores = [
     "orange",
     "green",
     "yellow",
-    "blue",
+    "grey",
+    "brown",
+    "purple",
+    "black",
+    "pink",
 ]
+
+let divPrincipal = document.querySelector("#principal")
+let pNome_Da_Cor = document.querySelector("#nome_da_cor")
+let index = 0
+
+pNome_Da_Cor.innerText = cores[index]
+
+function proximo(){
+    index += 1
+    if (index > cores.length - 1){
+        index = 0
+    }
+    pNome_Da_Cor.innerText = cores[index]
+    divPrincipal.style.backgroundColor = cores[index]
+
+}
+function anterior(){
+    index -= 1
+    if (index < 0){
+        index = cores.length - 1
+    }
+    pNome_Da_Cor.innerText = cores[index]
+    divPrincipal.style.backgroundColor = cores[index]
+}
